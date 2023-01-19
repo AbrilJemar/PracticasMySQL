7b39379bca8f787d721cb1923d2637e191861734
AbrilJemar <121254254+AbrilJemar@users.noreply.github.com>
2023-01-18 21:39:21 -0300

    Update PEDIDOS.sql

--- PEDIDOS.sql
+++ PEDIDOS.sql
@@ -5,25 +5,24 @@ CREATE TABLE pedidos (
     	fecha_envio date,
     	correo varchar(50),
     	nro_envio varchar(50),
+    	domicilio varchar(100),
     	ciudad_envio varchar(50),
     	PRIMARY KEY (id_pedido),
 	FOREIGN KEY(id_cliente) REFERENCES clientes(id_cliente)
 );
 
-SELECT * FROM pedidos;
-
-INSERT INTO pedidos (id_cliente, id_empleado, fecha_pedido, fecha_envio, correo, nro_envio, ciudad_envio)
+INSERT INTO pedidos (id_cliente, fecha_pedido, fecha_envio, correo, nro_envio, ciudad_envio)
 	VALUES
-    (1, 3, 20220411, 20220417, 'Andreani S.A', 360000681733540, 'Buenos Aires');
-    (5, 3, 20220805, 20220820, 'OCA Envios', 3654000000000480355, 'Buenos Aires, CABA'),
-    (2, 2, 20230106, 20230115, 'Andreani S.A', 3600007692837400, 'Pilar'),
-    (3,4, 20201228, 20210110, 'Jose MOTO MENSAJERIA', 20847362, 'Merlo' ),
-    (1, 5, 20200728, 20200809, 'Andreni S.A', 3600000076490324, 'Lomas del Mirador'),
-    (3, 1, 20180612, 20200620, 'MOTO MENSAJERIA SAN PEDRO', 782233012, 'Sam Miguel del Monte');
+    (1, 20220411, 20220417, 'Andreani S.A', 360000681733540, 'Buenos Aires'),
+    (5, 20220805, 20220820, 'OCA Envios', 3654000000000480355, 'Buenos Aires, CABA'),
+    (2, 20230106, 20230115, 'Andreani S.A', 3600007692837400, 'Pilar'),
+    (3, 20201228, 20210110, 'Jose MOTO MENSAJERIA', 20847362, 'Merlo' ),
+    (1, 20200728, 20200809, 'Andreni S.A', 3600000076490324, 'Lomas del Mirador'),
+    (3, 20180612, 20200620, 'MOTO MENSAJERIA SAN PEDRO', 782233012, 'Sam Miguel del Monte');
     
 
 UPDATE pedidos 
     SET ciudad_envio = 'Buenos Aires, CABA' 
     WHERE nro_envio = 360000681733540;
     
-    SELECT * FROM pedidos;
+SELECT * FROM pedidos;
